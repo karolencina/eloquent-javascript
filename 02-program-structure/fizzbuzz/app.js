f322da425f3a34d22cc5842aeb163ffc1f2d86c4
Karolina Lencina <karolencina@icloud.com>
2022-09-04 13:04:07 +0100

    Completed "FizzBuzz" project

---
+++ 02-program-structure/fizzbuzz/app.js
@@ -0,0 +1,11 @@
+for (let num = 0; num < 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log(num, 'FizzBuzz');
+    } else if (num % 3 === 0) {
+        console.log(num, 'Fizz');
+    } else if (num % 5 === 0) {
+        console.log(num, 'Buzz');
+    } else {
+        console.log(num)
+    }
+}
